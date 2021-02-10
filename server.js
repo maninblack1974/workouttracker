44cfdb942213f713168c55feda0cac670779ab0f
DRH <25594179+maninblack1974@users.noreply.github.com>
2021-02-09 23:09:24 -0800

    Adding file structure and files needed for project

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+const db = require("./models");
+
+require("./routes/api")(app);
+require("./routes/html")(app);
+
+app.listen(PORT,function(){ 
+    console.log(`App listening on Port ${PORT}`);
+});
